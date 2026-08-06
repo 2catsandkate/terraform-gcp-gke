aedb0c39eba254225da0ab81719ca113475125e0
2catsandkate <2cats.and.kate@gmail.com>
2026-08-06 20:28:44 +0000

    added important services

---
+++ locals.tf
@@ -0,0 +1,13 @@
+locals {
+  gcp_services = [
+    "compute.googleapis.com",
+    "dns.googleapis.com",
+    "storage-api.googleapis.com",
+    "container.googleapis.com",
+    "file.googleapis.com",
+    "artifactregistry.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "containeranalysis.googleapis.com",
+    "run.googleapis.com",
+  ]
+}
